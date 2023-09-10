1da2e2dfb201aff0603aa64027126b23193cde8a
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-09-10 21:31:13 +0600

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -79,3 +79,38 @@ class RegionalBank(Bank):
   
 my_bank = RegionalBank("Bank of America", 9, accounts)
 print(my_bank.regional_total())
+
+# Example 05 
+# The first parent is the Person class with some very generic information. The second class, CardHolder, is the class for a credit card holder. Create the child class PlatinumClient. This class inherits all of the attributes of both parent classes. In addition, the child class has the attributes cash_back and rewards. cash_back should be set to 0.02 and rewards should be set to 0. Override the process_sale method so that 2% of each sale is added to rewards.
+# parent classes
+class Person:
+  def __init__(self, name, address):
+    self.name = name
+    self.address = address
+    
+  def get_info(self):
+    return f"{self.name} lives at {self.address}."
+  
+class CardHolder:
+  def __init__(self, account_number):
+    self.account_number = account_number
+    self.balance = 0
+    self.credit_limit = 5000
+    
+  def process_sale(self, price):
+    self.balance += price
+    
+  def make_payment(self, amount):
+    self.balance -= amount
+    
+# declare child class here
+class PlatinumClient(Person, CardHolder):
+  def __init__(self, name, address, account_number):
+    Person.__init__(self, name, address)
+    CardHolder.__init__(self, account_number)
+    self.cash_back = 0.02
+    self.rewards = 0
+    
+  def process_sale(self, price):
+    self.balance += price
+    self.rewards += self.cash_back * price
