f858038f7fe1ed305ebe08885a084d6e8991132e
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-09-10 21:17:04 +0600

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -51,3 +51,31 @@ class Child(Parent2, Parent1):
   
   def identify2(self):
     return super().identify()
+
+# Example 04
+import sys
+strings = [l.split(",") for l in sys.argv[1].split("*")]
+accounts = [[int(n) for n in s] for s in strings]
+
+class Bank:
+  def __init__(self, name, customers, accounts):
+    self.name = name
+    self.customers = customers
+    self.accounts = accounts
+    
+  def branch_total(self, accounts):
+    total = 0
+    for account in accounts:
+      total += account
+    return total
+
+# Write your code here
+class RegionalBank(Bank):
+  def regional_total(self):
+    total = 0
+    for account in accounts:
+      total += self.branch_total(account)
+    return total
+  
+my_bank = RegionalBank("Bank of America", 9, accounts)
+print(my_bank.regional_total())
