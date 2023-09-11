54e4a48fca34c1e2c319895491b4f4e65df5cf79
Al-Amin Ifty <ifty.alamin54@gmail.com>
2023-09-12 01:28:06 +0600

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -27,3 +27,33 @@ c = Person("Papon")
 print(c.name)
 c.name = "Ifty"
 print(c.name)
+
+# Ex 02
+class Person:
+  def __init__(self, name, age):
+    self._name = name
+    self._age = age
+  
+  @property
+  def name(self):
+    return self._name
+  
+  @name.setter
+  def name(self, new_name):
+    self._name = new_name
+    
+  @property
+  def age(self):
+    return self._age
+  
+  @age.setter
+  def age(self, new_age):
+    self._age = new_age
+  
+c = Person("Calvin", "6")
+print(c.name)
+print(c.age)
+c.age = -17
+c.name = False
+print(c.name)
+print(c.age)
